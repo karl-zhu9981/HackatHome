fecece204f6918de90dec14cf01c52021b7512aa
Dmitry Tsarapkine <32915731+Dimatsa@users.noreply.github.com>
2020-05-02 16:42:11 -0400

    Use speech.js in backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const extractAudio = require('ffmeg-extract-audio');
+const speech = require('./speech.js')
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -15,15 +17,23 @@ const uri = "mongodb+srv://hackathome:" + process.env.DB_PASS + "@hackathome-yea
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  
+
   // perform actions on the collection object
   client.close();
 });
 
 // add a  
-app.get('/submit-video', (req, res, next) => {
-  console.log(req.query.videoURL);
-  res.send('ge');
+app.post('/submit-video', async (req, res, next) => {
+  videoPath = req.params.video.path;
+
+  // Extracts audio from video
+  const mp3AudioStream = await extractAudio({
+    input: videoPath,
+    output: 'audio.mp3'
+  });
+
+  const transcript = speech.transcription(mp3AudioStream);
+
 });
 
 app.use(express.static(__dirname + '/public'));
