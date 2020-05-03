5006157b0ba1abb6a9112449c51cdc28a10ebd98
eandr127 <eandr127@outlook.com>
2020-05-03 08:50:04 -0400

    Added highlighting/notes

--- server/client/src/TextAnnotater.jsx
+++ server/client/src/TextAnnotater.jsx
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components'
 import MonacoEditor from 'react-monaco-editor';
 import ToggleButton from 'react-bootstrap/ToggleButton'
+import Button from 'react-bootstrap/Button'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 
 const StyledTextAnnotater = styled.div`
     text-align: left;
+
+    .hidden {
+        display: none;
+    }
+
+    .highlight {
+        background: yellow !important;
+        color: black !important;
+        font-weight: bold;
+        font-style: oblique;
+    }
+
+    .note {
+        color: blue !important;
+        text-decoration: underline;
+        font-weight: bold;
+        font-style: oblique;
+    }
 `
 
 export default ({document}) => {
+    const monaco = useRef()
     const [selectedTool, setSelectedTool] = useState(1)
+    const [note, setNote] = useState("")
+    const [allNotes, setAllNotes] = useState([])
 
-    const handleChange = (val) => setSelectedTool(val);
+    const handleChange = (val) => { setNote("")
+    setSelectedTool(val) };
 
     return <StyledTextAnnotater >
             <ToggleButtonGroup  type="radio" name="options" value={selectedTool} onChange={handleChange}>
@@ -19,7 +42,22 @@ export default ({document}) => {
                   <ToggleButton value={2}>Highlighter</ToggleButton>
                 <ToggleButton value={3}>Notepad</ToggleButton>
             </ToggleButtonGroup>
+            <Button onClick={() => {
+                const selection = monaco.current.editor.getSelection();
+                monaco.current.editor.deltaDecorations([], [
+                    { range: selection, options: { inlineClassName: selectedTool === 2 ? 'highlight' : 'note'}, hoverMessage: selectedTool === 3 ? { value: note, isTrusted: true} : undefined}
+                ]);
+
+                if(selectedTool === 3 && note)
+                setAllNotes([...allNotes, note])
+                setNote("")
+            }} className={selectedTool === 1 ? "hidden" : ""}>{selectedTool === 2 ? "Highlight" : "Make Note"}</Button>
+
+            <input value={note} className={selectedTool === 3 ? "" : "hidden"} onChange={(evt) => setNote(evt.target.value)} placeholder="Note contents" />
+            
+            {allNotes.map((val, idx) => <ul key={idx}>{val}</ul>)}
             <MonacoEditor
+            ref={monaco}
             width="800"
             height="600"
             language="javascript"
@@ -28,7 +66,7 @@ export default ({document}) => {
                 readOnly: true,
                 lineNumbers: "off",
                 renderIndentGuides: false,
-                autoIndent: "none"
+                autoIndent: "none",
             }}
             value={document.text}
         />
