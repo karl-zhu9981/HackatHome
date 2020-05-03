4123303202f68e1d2d5b38d31035e32c9376dc99
eandr127 <eandr127@outlook.com>
2020-05-03 06:01:05 -0400

    Add radio buttons for annotation tools

--- server/client/src/TextAnnotater.jsx
+++ server/client/src/TextAnnotater.jsx
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
 import styled from 'styled-components'
 import MonacoEditor from 'react-monaco-editor';
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 
 const StyledTextAnnotater = styled.div`
-    text-align: left
+    text-align: left;
 `
 
 export default ({document}) => {
-    return <StyledTextAnnotater>
-        <MonacoEditor
-        width="800"
-        height="600"
-        language="javascript"
-        theme="vs-dark"
-        options={{
-            readOnly: true,
-            lineNumbers: "off",
-            renderIndentGuides: false,
-            autoIndent: "none"
-        }}
-        value={document.text}
-      />
+    const [selectedTool, setSelectedTool] = useState(1)
+
+    const handleChange = (val) => setSelectedTool(val);
+
+    return <StyledTextAnnotater >
+            <ToggleButtonGroup  type="radio" name="options" value={selectedTool} onChange={handleChange}>
+                 <ToggleButton value={1}>None</ToggleButton>
+                  <ToggleButton value={2}>Highlighter</ToggleButton>
+                <ToggleButton value={3}>Notepad</ToggleButton>
+            </ToggleButtonGroup>
+            <MonacoEditor
+            width="800"
+            height="600"
+            language="javascript"
+            theme="vs-dark"
+            options={{
+                readOnly: true,
+                lineNumbers: "off",
+                renderIndentGuides: false,
+                autoIndent: "none"
+            }}
+            value={document.text}
+        />
     </StyledTextAnnotater>
 }
