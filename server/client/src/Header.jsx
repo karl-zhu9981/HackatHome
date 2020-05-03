8a89c1508d107c23411a5a21d0d6a3ec638ac0b2
eandr127 <eandr127@outlook.com>
2020-05-03 07:15:42 -0400

    Fix CSS syntax errors

--- server/client/src/Header.jsx
+++ server/client/src/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components'
 
 const StyledHeader = styled.div`
- margin-top: 1%
- margin-bottom: 2%
+ margin-top: 1%;
+ margin-bottom: 2%;
 `
 
 export default () =>
